d88dc18a43897af121c4e9850fdb9f4d13d2c335
unknown <khorshedjhoni@gmail.com>
2022-10-25 21:56:28 +0600

    faq part added

--- src/Components/FAQ/FAQ.js
+++ src/Components/FAQ/FAQ.js
@@ -3,7 +3,18 @@ import React from 'react';
 const FAQ = () => {
     return (
         <div>
-            <h2>this is faq</h2>
+            <div>
+            <h5>What are the technology requirements to take this course?</h5>
+            <p>This course design for beginner to advanced level student.So any one who can operate the computer can access this course</p>
+            </div>
+            <div>
+                <h5>What does the course number mean?</h5>
+                <p>The course number designates the level of instruction for which the course is intended.</p>
+            </div>
+            <div>
+                <h5>Can I find out more about these courses?</h5>
+                <p>The courses are detailed in the course offerings. For further information, please visit our website.</p>
+            </div>
         </div>
     );
 };
